ff8e9ceae3281ca184ca44355a4c3c9d1baa31f0
estefanofc <estefano_fc@hotmail.com>
2020-04-24 14:18:23 -0700

    initial setup

---
+++ lib/menu_dashboard_layout.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+
+final Color backgroundColor = Color(0xFF4A4A58);
+
+class MenuDashboardPage extends StatefulWidget {
+  @override
+  _MenuDashboardPageState createState() => _MenuDashboardPageState();
+}
+
+class _MenuDashboardPageState extends State<MenuDashboardPage> {
+  bool isCollapsed = true;
+  double screenHeight, screenWidth;
+  final Duration duration = const Duration(milliseconds: 3000);
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    screenHeight = size.height;
+    screenWidth = size.width;
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      body: Stack(
+        children: <Widget>[
+          menu(context),
+          dashboard(context),
+        ],
+      ),
+    );
+  }
+
+  Widget menu(context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 16.0),
+      child: Align(
+        alignment: Alignment.centerLeft,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'Dashboard',
+              style: TextStyle(color: Colors.white, fontSize: 22),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Messagges',
+              style: TextStyle(color: Colors.white, fontSize: 22),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Profile',
+              style: TextStyle(color: Colors.white, fontSize: 22),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Friends',
+              style: TextStyle(color: Colors.white, fontSize: 22),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Find',
+              style: TextStyle(color: Colors.white, fontSize: 22),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget dashboard(context) {
+    return AnimatedPositioned(
+      duration: duration,
+      top: isCollapsed ? 0 : 0.2 * screenHeight,
+      bottom: isCollapsed ? 0 : 0.2 * screenHeight,
+      left: isCollapsed ? 0 : 0.6 * screenWidth,
+      right: isCollapsed ? 0 : -0.4 * screenWidth,
+      child: Material(
+        animationDuration: duration,
+        borderRadius: BorderRadius.all(Radius.circular(40)),
+        elevation: 8,
+        color: backgroundColor,
+        child: Container(
+          padding: const EdgeInsets.only(left: 16, right: 16, top: 48),
+          child: Column(
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  InkWell(
+                    child: Icon(Icons.menu, color: Colors.white),
+                    onTap: () {
+                      setState(() {
+                        isCollapsed = !isCollapsed;
+                      });
+                    },
+                  ),
+                  Text("My Cards",
+                      style: TextStyle(fontSize: 24, color: Colors.white)),
+                  Icon(Icons.settings, color: Colors.white),
+                ],
+              ),
+              Container(
+                height: 200,
+                child: PageView(
+                  controller: PageController(viewportFraction: 0.8),
+                  scrollDirection: Axis.horizontal,
+                  pageSnapping: true,
+                  children: <Widget>[
+                    Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 8),
+                      color: Colors.redAccent,
+                      width: 100,
+                    ),
+                    Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 8),
+                      color: Colors.blueAccent,
+                      width: 100,
+                    ),
+                    Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 8),
+                      color: Colors.greenAccent,
+                      width: 100,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
